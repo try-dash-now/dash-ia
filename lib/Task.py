67a281b2c3341fc37a5fb2d30f72c5d6c5f9b9ce
Sean Yu <try.dash.now@gmail.com>
2015-05-21 20:05:15 +0800

    create html test report for a test suite

--- lib/Task.py
+++ lib/Task.py
@@ -43,9 +43,7 @@ class Task(object):
     CaseRangeStr= ''
     ArgStr= ''
     Report=[['index', 'case','TCP port', 'logdir', 'pid', 'start time', 'end time','duration','result']]
-    def __init__(self, config=None):
-        if not config:
-            config='./manualrun.cfg'
+    def __init__(self, config='./manualrun.cfg'):
         from common import csvfile2dict
         self.RunCfg = csvfile2dict(config)
         import time
@@ -62,8 +60,8 @@ class Task(object):
             self.SuiteFile = SuiteFile
         else:
             self.SuiteFile = self.RunCfg['suitedir']+os.path.sep+SuiteFile
-        
-        with open(self.SuiteFile, newline='') as f:
+
+        with open(self.SuiteFile) as f:#, newline=''
             suitestring = f.read()
             index = 0
             temp = args.split(',')[1:]
@@ -74,11 +72,11 @@ class Task(object):
             print(suitestring)
         from common import csvstring2array
         self.CaseList = csvstring2array(suitestring)
-        
-        
+
+
         response = '%s(%s):completed'%(sys._getframe().f_code.co_name, self.SuiteFile)
         self.CaseRangeStr = caserange
-        self.SetRange(caserange)  
+        self.SetRange(caserange)
         return response
     def LoadCaseInArray(self,suitname, caselist=[], caserange ='ALL',args=''):
         self.ArgStr=args
@@ -89,7 +87,7 @@ class Task(object):
         self.SetRange(caserange)
         self.Report =[['index', 'case','TCP port', 'logdir', 'pid', 'start time', 'end time','duration','result']]
     def Run1Case(self, cmd, tcpport,  logdir, DebugWhenFailed=False):
-        from csv2case import csv2case 
+        from csv2case import csv2case
         import tempfile
         import re as sre
         cmd = cmd.strip()
@@ -99,42 +97,42 @@ class Task(object):
         if cmd.startswith('./run.p') or cmd.startswith('./t.p') or cmd.startswith('t.p'):
             pass
         else:
-            cmd = './html/case/%s'%cmd
+            cmd = '../case/%s'%cmd
         if str(cmd).find(".py") !=-1:
             rePythonVersion = sre.compile(".+?;([0-9]+\.[0-9]+)\s*$", sre.M)
             m = sre.match(rePythonVersion, str(cmd))
-            
+
             if m!=None:
                 #print str(m.group(1))
                 cmd = str(cmd)[ 0:str(cmd).find(";")]
                 exe_cmd = "python%s "%(str(m.group(1)))
             else:
-                exe_cmd = "python3 "
+                exe_cmd = "python "
         else:
             exe_cmd = " "
-        #print exe_cmd  
+        #print exe_cmd
         import time
-        sst =time.time() 
+        sst =time.time()
         st = time.localtime()
         starttime =time.strftime("%Y-%m-%d %H:%M:%S", st)
-        from Database import FetchOne, UpdateRecord, InsertRecord
+        #from Database import FetchOne, UpdateRecord, InsertRecord
         dbname = self.RunCfg.get('db')
         caseinfo = self.RunCfg.get('caseinfo')
-  
+
         import subprocess
         pp =None
         if DebugWhenFailed:
             DebugWhenFailed='True'
         else:
             DebugWhenFailed= 'False'
-        
-        if cmd.find('t.py') !=-1 :  #cmd.startswith('r.py ') or 
+
+        if cmd.find('t.py') !=-1 :  #cmd.startswith('r.py ') or
             exe_cmd =exe_cmd+ cmd+" -l "+logdir
             pp = subprocess.Popen(args = exe_cmd ,shell =True, stdin=pipe_input)
         else:
             exe_cmd = exe_cmd+ cmd+" %d %s "%(tcpport, DebugWhenFailed)+logdir
-            pp = subprocess.Popen(args = exe_cmd,shell =True, stdin=pipe_input)  
-        InsertRecord(dbname, caseinfo, """%f, 0.0, %d,  '%s', 'started'"""%(sst, pp.pid,  exe_cmd ))
+            pp = subprocess.Popen(args = exe_cmd,shell =True, stdin=pipe_input)
+        #InsertRecord(dbname, caseinfo, """%f, 0.0, %d,  '%s', 'started'"""%(sst, pp.pid,  exe_cmd ))
 
         self.Current.update({'5-pid':pp.pid, '6-starttime': starttime})
         self.Report[self.Report.__len__()-1][4]=pp.pid
@@ -147,14 +145,14 @@ class Task(object):
                 interval = 1
                 if first:
                     first=False
-                    UpdateRecord(dbname, caseinfo, """status='running'""", 'start_time=%f and pid= %d'%(sst, pp.pid))
+                    #UpdateRecord(dbname, caseinfo, """status='running'""", 'start_time=%f and pid= %d'%(sst, pp.pid))
 
                 time.sleep(interval)
             else:
                 #return pp.returncode:
-                ChildRuning = False  
+                ChildRuning = False
         et = time.localtime()
-        eet =time.time()        
+        eet =time.time()
         duration = '%.2f'%(eet-sst)
         self.Report[self.Report.__len__()-1][7]=duration
         et= time.strftime("%Y-%m-%d %H:%M:%S", et)
@@ -163,7 +161,7 @@ class Task(object):
         result='pass'
         if pp.returncode:
             result='fail'
-        UpdateRecord(dbname, caseinfo, """status='%s', end_time=%f"""%(result,eet), 'start_time=%f and pid= %d'%(sst, pp.pid))
+        #UpdateRecord(dbname, caseinfo, """status='%s', end_time=%f"""%(result,eet), 'start_time=%f and pid= %d'%(sst, pp.pid))
 
         self.Current.update({'8-result':result})
         self.Report[len(self.Report)-1][8]=result
@@ -174,15 +172,16 @@ class Task(object):
     def Run(self, server=None):
         '''start to run cases in pole'''
         subdirname = time.strftime("%Y%m%d_%H%M%S", time.localtime())
-        dirname = os.path.basename(self.SuiteFile)       
+        dirname = os.path.basename(self.SuiteFile)
         dirname = "%s%s%s"%(self.RunCfg['logdir'], os.path.sep,  dirname.replace(',', '_').replace('-', '_'))
         subdirname= "%s%s%s"%(dirname,os.path.sep, subdirname.replace(',', '_').replace('-', '_'))
+        dirname = os.path.abspath(dirname)
         if not os.path.exists(dirname):
             os.mkdir(dirname)
         if not os.path.exists(subdirname):
             os.mkdir(subdirname)
         #index = 0
-        
+
         self.PASS=0
         self.FAIL=0
         self.SKIP =0
@@ -192,17 +191,18 @@ class Task(object):
             self.Current.update({
                      '1-index':-1,
                      '2-case':'',
-                     '3-tcpport':-1,                     
-                     '4-logdir':'',                      
-                     '5-pid':-1,                    
+                     '3-tcpport':-1,
+                     '4-logdir':'',
+                     '5-pid':-1,
                      '6-starttime':'',
                      '7-endtime':'',
                      '8-result':''
                      }
                     )
-            
-            self.Report.append([self.index+1, '', -1, '','0',0.0,0.0,'-',''])
+
+
             if self.InRange(self.index):
+                self.Report.append([self.index+1, '', -1, '','0',0.0,0.0,'-',''])
                 pass
             elif len(self.CaseList)<self.index:
                 self.index= self.index+1
@@ -224,8 +224,8 @@ class Task(object):
                 item.append('')
             elif len(item)==2:
                 item.append('')
-            self.Current.update({'1-index':self.index+1, '2-case':item})    
-            self.Report[len(self.Report)-1][1]=item 
+            self.Current.update({'1-index':self.index+1, '2-case':item})
+            self.Report[len(self.Report)-1][1]=item
         #for item in self.CaseList:
             case =item[0]
             if server==None:
@@ -233,10 +233,10 @@ class Task(object):
             else:
                 dbname =self.RunCfg['db']
                 tcpportpool = self.RunCfg['tcppool']
-                
-                       
-                tcpport = FetchOne(dbname, tcpportpool, 'status="idle"')           
-                
+
+
+                tcpport = FetchOne(dbname, tcpportpool, 'status="idle"')
+
                 if not tcpport:
                     self.STATUS= 'Index#%d (1 based) is going to be ran, no TCP port is idle, waiting for an avaiable TCP'%(self.index+1)
                     time.sleep(1)
@@ -247,7 +247,7 @@ class Task(object):
             #self.Report[self.Report.__len__()-1][1]=item
             if server:
                 UpdateRecord(dbname,tcpportpool,'status="in-used"','port=%d'%int(tcpport))
-            
+
 #===============================================================================
 # start_time
 # double    end_time
@@ -263,19 +263,19 @@ class Task(object):
             if not os.path.exists(casedir):
                 os.mkdir(casedir)
             self.Current.update({'4-logdir':casedir})
-            self.Report[len(self.Report)-1][3]=casedir            
+            self.Report[len(self.Report)-1][3]=casedir
             returncode = self.Run1Case(case, tcpport, casedir)
 
-            
+
             if returncode:
                 if len(item)>2:
-                    mTry = sre.match(sreTry,item[2])                    
+                    mTry = sre.match(sreTry,item[2])
                     if mTry:
-                        counter = mTry.group(1)
+                        counter = int(mTry.group(1))
                         while counter:
                             returncode = self.Run1Case(case, tcpport , casedir)
                             if returncode:
-                                counter=-1
+                                counter-=1
                             else:
                                 self.PASS=self.PASS+1
                                 break
@@ -286,18 +286,18 @@ class Task(object):
             else:#case passed
                 self.PASS=self.PASS+1
                 pass
-            
+
             mAbort = sre.match(sreAbort,item[1])
             print('CURRENT STATUS',self.Current)
             if server:
                 UpdateRecord(dbname,tcpportpool,'status="idle"','port=%d'%int(tcpport))
             self.DumpReport()
-            if returncode and mAbort:               
-                break        
-           
+            if returncode and mAbort:
+                break
+
             self.index =self.index+1
 
-        
+
         self.fRunning=False
     def StartTask(self, index):
         self.fStopTask=False
@@ -324,8 +324,8 @@ class Task(object):
         self.fStopTask=True
         if KillCurrentCase:
             self.killCase()
-        
-        
+
+
     def CancelTask(self, KillCurrentCase=True):
         '''stop this task, kill current case, if KillCurrentCase==True by default, and cancel all remaining cases'''
         self.fCanncelTask=True
@@ -335,43 +335,47 @@ class Task(object):
         UpdateRecord(dbname,tcpportpool,'status="idle"','port=%d'%int(self.Current['3-tcpport']))
         if KillCurrentCase:
             self.killCase()
-        self.DumpReport()    
+        self.DumpReport()
         time.sleep(1)
-        
+
     def DumpReport(self):
         try:
             print('Report:\n')
             print('-'*80)
-            print('\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s'%(self.Report[0][0], 
-                                                          self.Report[0][8],
-                                                          self.Report[0][2],
-                                                          self.Report[0][4],
-                                                          self.Report[0][7],
-                                                          str(self.Report[0][1]),
-                                                          self.Report[0][3],
-                                                          self.Report[0][5],
-                                                          self.Report[0][6]))
+            line = '\t'+'\t'.join(self.Report[0])
+            print(line)
+            # print('\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s'%(self.Report[0][0],
+            #                                               self.Report[0][8],
+            #                                               self.Report[0][2],
+            #                                               self.Report[0][4],
+            #                                               self.Report[0][7],
+            #                                               str(self.Report[0][1]),
+            #                                               self.Report[0][3],
+            #                                               self.Report[0][5],
+            #                                               self.Report[0][6]))
             for r in self.Report[1:]:
                 try:
-                    print('\t%d\t%s\t%d\t%d\t%s\t%s\t%s\t%s\t%s'%(int(r[0]), 
-                                                              r[8],
-                                                              int(r[2]),
-                                                              int(r[4]),
-                                                              r[7],
-                                                              str(r[1]),
-                                                              r[3],
-                                                              r[5],
-                                                              r[6])
-                      )
+                    line = '\t'+'\t'.join([ str(i) for i in r])
+                    print(line)
+                    # print('\t%d\t%s\t%d\t%d\t%s\t%s\t%s\t%s\t%s'%(int(r[0]),
+                    #                                           r[8],
+                    #                                           int(r[2]),
+                    #                                           int(r[4]),
+                    #                                           r[7],
+                    #                                           str(r[1]),
+                    #                                           r[3],
+                    #                                           r[5],
+                    #                                           r[6])
+                    #   )
                 except Exception as e:
                     print(e)
-                    
+
                 #print('')
             print('-'*80)
             report =self.GenerateHtmlReport()
-            htmlfile = "./html/report/%s_%s_%s_%s.html"%(self.SuiteFile,'_'.join(self.ArgStr), self.CaseRangeStr, self.TaskStartTime)
+            htmlfile = "%s_%s_%s.html"%('_'.join(self.ArgStr), self.CaseRangeStr, self.TaskStartTime)
             htmlfile=htmlfile.replace('-', '_').replace(',', '_').replace('/', '_')
-            with open(htmlfile, 'wb') as f:
+            with open("%s/%s_%s"%( self.RunCfg['report'],os.path.basename(self.SuiteFile),htmlfile), 'wb') as f:
                 f.write(report.encode(encoding='utf_8', errors='strict'))
         except Exception as e:
             print("Task::DumpReport() error: "+str(e))
@@ -379,7 +383,7 @@ class Task(object):
         TOTAL = len(self.CaseRange)
         CASEPASS = self.PASS
         CASEFAIL = self.FAIL
-        
+
         CASERUN = CASEPASS +CASEFAIL
         if CASERUN==0:
             CASERUN=1
@@ -390,7 +394,7 @@ class Task(object):
         CASENOTRUN  = TOTAL - CASEPASS-CASEFAIL
         PNOTRUN = '%.0f'%((CASENOTRUN /TOTAL)*100.0)+'''%'''
 
-         
+
         response ="""
 <HTML>
 <HEAD>
@@ -418,8 +422,8 @@ class Task(object):
 <BR>
 <table cellspacing="1" cellpadding="2" border="1">
 """%(self.SuiteFile, self.ArgStr, self.CaseRangeStr ,TOTAL, CASEPASS, CASEFAIL, CASENOTRUN, PPASS,PFAIL,PNOTRUN)
-        
-        response = response+ '''<tr><td>No.</td><td>Result</td><td>Case Name</td><td>Duration(s)</td></tr>'''        
+
+        response = response+ '''<tr><td>No.</td><td>Result</td><td>Case Name</td><td>Duration(s)</td></tr>'''
         if len(self.Report)<2:
             return response+"""no Case result in this Task</table>
 <br />
@@ -429,13 +433,13 @@ class Task(object):
             bgcolor="#00FF00"
             if result[8]=='fail':
                 bgcolor = "#FF0000"
-            
+
             response = response +"""<tr><td>%d</td><td bgcolor="%s"><a target="+BLANK" href="%s">%s</td><td><a target="+BLANK" href="%s">%s</td><td>%s</td></tr>
 """%(result[0],bgcolor,result[3][1:],result[8],result[3][1:],result[1][0],result[7])
 
 
 
-        
+
         return response+"""</table>
 <br />
 <br />
@@ -444,7 +448,7 @@ class Task(object):
         '''kill running case'''
         if self.Current['5-pid']!=-1:
             try:
-                
+
                 os.kill(self.Current['5-pid'], signal.SIGTERM)
                 self.Report[self.Report.__len__()-1][6]='Canceled'
             except Exception as e:
@@ -462,7 +466,7 @@ class Task(object):
     def IsAlive(self):
         return self.fRunning
     def InRange(self,index):
-        try: 
+        try:
             self.CaseRange.index(index)
             return True
         except Exception as e:
@@ -470,10 +474,10 @@ class Task(object):
     def SetRange(self,caserange='ALL'):
         if not self.CaseList:
             return []
-            
+
         if str(caserange).upper()=='ALL':
             caserange = range(0, len(self.CaseList)-1)
-        else:        
+        else:
             caserange = str(caserange).strip()
             caserange = str(caserange).split(',')
             drange = []
@@ -487,6 +491,4 @@ class Task(object):
             caserange= sorted(drange)
             self.CaseRange=caserange
             return self.CaseRange
-        
-
 
